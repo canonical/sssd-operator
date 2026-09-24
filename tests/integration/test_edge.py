2e5e42a03128706f2b0c151627477301ac990525
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2026-09-24 17:15:02 -0400

    fix: use `parsers.re` so that statement with empty expected results will match

--- tests/integration/test_edge.py
+++ tests/integration/test_edge.py
@@ -68,7 +68,7 @@ def configure_ldap_integrator(context: Context) -> None:
     )
 
 
-@then(parsers.parse("the output should be '{expected}'"))
+@then(parsers.re("the output should be '(?P<expected>[^']*)'"))
 def assert_output(context: Context, expected: str) -> None:
     """Assert the most recent exec result matches the expected output."""
     task = context.exec_results.peek()
